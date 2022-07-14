24b61b66ec55c80906fccc2409a26392d3c0e200
Marcelo Fernandez <marcelo.fernandez@incrowdsports.com>
2022-07-13 21:30:11 -0300

    Updated project with some improvements in the constraints and the details screen.

--- MVVMWithClosures/Details/DetailsViewController.swift
+++ MVVMWithClosures/Details/DetailsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DetailsViewController: UIViewController {
     var bandId = 0
     
+    let activityIndicator = UIActivityIndicatorView()
     let detailsViewModel = DetailsViewModel()
     @IBOutlet weak var bandName: UILabel!
     @IBOutlet weak var bandThumbnail: UIImageView!
@@ -22,19 +23,52 @@ class DetailsViewController: UIViewController {
         navigationController?.navigationBar.backgroundColor = .black
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationItem.title = "Information"
-        self.bandThumbnail.image = UIImage(named: "placeholder")
+        setDefaultValues()
+        configureActivityIndicator()
         bind()
         detailsViewModel.getBandData(with: bandId)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setConstraints()
     }
     
     private func bind() {
         detailsViewModel.refreshData = { [weak self] in
             DispatchQueue.main.async {
+                self?.activityIndicator.stopAnimating()
                 self?.bandName.text = self?.detailsViewModel.bandData?.name
                 self?.bandThumbnail.load(url: self?.detailsViewModel.bandData?.image)
                 self?.bandGenre.text = self?.detailsViewModel.bandData?.genre?.rawValue
                 self?.bandInformation.text = self?.detailsViewModel.bandData?.info
             }
         }
+    }
+    
+    private func setDefaultValues() {
+        self.bandThumbnail.image = UIImage(named: "placeholder")
+        self.bandName.text = ""
+        self.bandGenre.text = ""
+        self.bandInformation.text = ""
+    }
+    
+    private func configureActivityIndicator() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = .black
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+    
+    private func setConstraints() {
+        let constraints = [
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.widthAnchor.constraint(equalToConstant: 40),
+            activityIndicator.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
 }
